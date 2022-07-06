94b2377f4a6f30c26c8783e44f0f1de5f574e4e7
brian.beardsley.89123@gmail.com <94084517+Bevelled@users.noreply.github.com>
2022-07-05 20:46:34 -0700

    v1.0 hosted 777detail.com

--- hostedFiles/assets/js/main.js
+++ hostedFiles/assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 /* To navigate out of the gallery lightbox */
 document.querySelector('body').addEventListener('click', e => {	
